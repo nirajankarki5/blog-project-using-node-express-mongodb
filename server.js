937947343f132d3f03bc7870397094e12f17fe7f
nirajankarki5 <nirajankarki5@gmail.com>
2021-11-07 11:35:20 -0800

    added mongoose and connected to db

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require('path');
+
+// dotenv.config({ path: './config.env' });
+dotenv.config({ path: path.resolve(__dirname, './config.env') });
+
 const app = require('./app');
 
-dotenv.config({ path: './config.env ' });
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB connection successful');
+  });
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
