8e56017bb646b378320518e790491f25962a1d8a
nirajankarki5 <nirajankarki5@gmail.com>
2021-11-07 12:50:26 -0800

    web updates

--- controllers/blog_controller.js
+++ controllers/blog_controller.js
@@ -26,8 +26,6 @@ exports.about = (req, res) => {
 
 exports.addNewBlog = async (req, res) => {
   try {
-    // you can simply use req.body but it got "[Object: null prototype] { ... }" so using this
-    console.log('body is ', JSON.parse(JSON.stringify(req.body)));
     const newBlog = await Blog.create(req.body);
     // redirect to add blog page once the blog is created
     res.status(201).redirect('/add-blog');
