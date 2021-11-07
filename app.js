d8fa9e71933b319e8130e9fc9f50a22b98517591
nirajankarki5 <nirajankarki5@gmail.com>
2021-11-07 08:25:57 -0800

    added views

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
 app.use(express.json());
 
 // set tem[late engine to ejs
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/blog/home", (req, res) => {});
+// set css folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  let blogList = [
+    { title: 'Blog 1', description: 'This is description of blog 1' },
+    { title: 'Blog 2', description: 'This is description of blog 2' },
+    { title: 'Blog 3', description: 'This is description of blog 3' },
+  ];
+  res.render('index.ejs', { blogList: blogList });
+});
+
+app.get('/add-blog', (req, res) => {
+  res.render('add_page.ejs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs');
+});
 
 module.exports = app;
